f094d90f8a80c851ffcab80f73b1d833ea90aa9d
DanGreinke <d.greinke@gmail.com>
2023-12-22 11:57:48 -0800

    render encyclopedia entries

--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
+import markdown
 
 
 def index(request):
@@ -8,3 +9,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def article(request, entry):
+    entry_markdown = util.get_entry(entry)
+    if entry_markdown:
+        return render(request, f"encyclopedia/article.html", {
+            "entry_html":markdown.markdown(entry_markdown)
+        })
+    else:
+        return HttpResponse("Entry not found :(")

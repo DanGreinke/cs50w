64f9e9fe442c924af65665a265bafe0ce2be44e0
DanGreinke <d.greinke@gmail.com>
2023-12-23 16:58:35 -0800

    Add search functionality, random page

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry>", views.article, name="article")
+    path("wiki/<str:entry>", views.article, name="article"),
+    path("random", views.random_page, name="random"),
+    path("search", views.search, name="search")
 ]
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import util
 import markdown
+import random
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 def article(request, entry):
     entry_markdown = util.get_entry(entry)
@@ -17,3 +19,36 @@ def article(request, entry):
         })
     else:
         return HttpResponse("Entry not found :(")
+
+
+def random_page(request):
+    random_entry = util.get_entry(random.choice(util.list_entries()))
+    return render(request, f"encyclopedia/article.html", {
+        "entry_html":markdown.markdown(random_entry)
+    })
+
+
+
+def search(request):
+    query = request.GET.get('q',"")
+    filtered_entries = list()
+    all_entries = util.list_entries()
+
+    for i in all_entries:
+        if query.lower() == i.lower():
+            return render(request, f"encyclopedia/article.html", {
+                "entry_html":markdown.markdown(util.get_entry(i))
+            })
+        elif query in i:
+            filtered_entries.append(i)
+        else:
+            continue
+
+    if filtered_entries:
+        return render(request, "encyclopedia/index.html", {
+            "entries": filtered_entries
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
